32868370ee9952dc76f5d48a6800d546536b839b
Dougherty, Joseph <josepd@wharton.upenn.edu>
2015-03-24 11:29:12 -0400

    include setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distuils.core import setup
+
+setup(
+        name="sasdataframe",
+        version='0.0.1',
+        description="Import a SAS dataset into a Pandas dataframe",
+        author="Joe Dougherty",
+        author_email="josepd@wharton.upenn.edu",
+        url="http://www.upenn.edu",
+        packages=['sasdataframe'],
+        install_requires=['inlinesas'],
+        )
+
