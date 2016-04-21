eb2882b589febb50e2358d0f2bcfc0a3c96f00d8
Dougherty, Joseph <josepd@wharton.upenn.edu>
2016-04-21 13:52:16 -0400

    Adds usage docstring to sas_to_dataframe function

--- sasdataframe/__init__.py
+++ sasdataframe/__init__.py
@@ -22,6 +22,26 @@ def create_pd_dataframe(abs_path_to_csv, type_dict={}):
         return pd.read_csv(abs_path_to_csv)
 
 def sas_to_dataframe(path_to_libname, dsname, abs_path_to_csv, type_dict={}):
+    """
+    path_to_libname [string]: 
+        Absolute path to the directory that the SAS dataset resides in.
+
+    dsname [string]:
+        Name of the SAS dataset (Ex: 'returns', 'columns').
+
+    abs_path_to_csv [string]:
+        Absolute path of csv file that the SAS dataset will be exported to.
+
+    type_dict [dictionary] (optional):
+        A dictionary of columns and their types.
+        See http://pandas.pydata.org/pandas-docs/stable/generated/pandas.read_csv.html for details.
+
+    USAGE:
+    ======
+
+    df = sas_to_dataframe('/path/to/sasdataset/directory', 'dataset_name', '/home/user/tempdataset.csv')
+
+    """
     return_obj = export_dsn_to_csv(path_to_libname, dsname, abs_path_to_csv, type_dict=type_dict)
 
     if return_obj.returncode not in (0,1):
