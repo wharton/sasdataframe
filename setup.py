541f2b120e8e955f8bd876a7c9586f772060fcef
wrdsadmn <josepd@wharton.upenn.edu>
2016-12-06 14:27:52 -0500

    Version bump;

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
         name="sasdataframe",
-        version='0.0.5',
+        version='0.0.6',
         description="Import a SAS dataset into a Pandas dataframe",
         author="Joe Dougherty",
         author_email="josepd@wharton.upenn.edu",
